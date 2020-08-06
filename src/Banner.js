1b5eccefd0b8084582bfc966749d8f7300eed792
Duan Chandrababu <Duan.Chandrababu@cammsgroup.com>
2020-08-07 00:16:26 +0530

    initial release

---
+++ src/Banner.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import requests from "./requests";
+import "./Banner.css";
+const Banner = () => {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    axios.get(requests.fetchNetflixOriginals).then((response) => {
+      setMovie(
+        response.data.results[
+          Math.floor(Math.random() * response.data.results.length - 1)
+        ]
+      );
+    });
+  }, []);
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
+  if (movie.backdrop_path) {
+    return (
+      <header
+        className="banner"
+        style={{
+          backgroundSize: "cover",
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+          backgroundPosition: "center center",
+        }}
+      >
+        <div className="banner_contents">
+          <h1 className="banner_title">
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
+          <div className="banner_buttons">
+            <button className="banner_button">Play</button>
+            <button className="banner_button">My List</button>
+          </div>
+          <h1 className="banner_description">
+            {truncate(movie?.overview, 150)}
+          </h1>
+        </div>
+        <div className="banner--fadebottom"></div>
+      </header>
+    );
+  } else {
+    return null;
+  }
+};
+
+export default Banner;
---
+++ src/Row.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const img_base_url = "https://image.tmdb.org/t/p/original/";
+
+const Row = (props) => {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  useEffect(() => {
+    axios.get(props.fetchUrl).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, [props.fetchUrl]);
+
+  const opts = {
+    height: "300",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const clickHandler = (movie) => {
+    debugger;
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || movie?.title)
+        .then((url) => {
+          debugger;
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((err) => {
+          debugger;
+
+          console.log(err);
+        });
+    }
+  };
+  return (
+    <div className="row">
+      {/* TITLE */}
+      <h2>{props.title}</h2>
+
+      {/* POSTERS */}
+      <div className="row_posters">
+        {movies.map((movie) => {
+          if (movie.poster_path != null && movie.backdrop_path != null) {
+            return (
+              <img
+                className={`row_poster ${
+                  props.isLargeRow && "row_posterLarge"
+                }`}
+                src={`${img_base_url}${
+                  props.isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+                key={movie.id}
+                onClick={() => clickHandler(movie)}
+              />
+            );
+          }
+        })}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+};
+
+export default Row;
